503d221a5e988d240e4742db31fbc16908091bd9
robsmithers <rob.smith@outlook.com>
2014-02-04 20:58:09 -0600

    grunt mod

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,4 +14,38 @@ module.exports = function(grunt) {
             }
         }
     });
+
+    // Load grunt-karma task plugin
+    grunt.loadNpmTasks('grunt-karma');
+    // Load the grunt-contrib-watch plugin for doing file watches
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('test', ['jshint', 'karma:unit']);
+
+    // Task for development; auto-build ng-grid.debug.js on source file changes, auto-test on ng-grid.debug.js or unit test changes
+    grunt.registerTask('testwatch', ['jshint', 'karma:watch', 'watch']);
+
+    grunt.registerTask('test-ci', ['jshint', 'debug', 'karma:ci']);
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    //grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-angular-templates');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    // Old default task
+    grunt.registerTask('build', ['less', 'ngtemplates', 'concat', 'uglify', 'clean']);
+
+    // Default task(s).
+    grunt.registerTask('default', 'No default task', function() {
+        grunt.log.write('The old default task has been moved to "build" to prevent accidental triggering');
+    });
+
+    grunt.registerTask('debug', ['less', 'ngtemplates', 'concat:debug', 'clean']);
+    grunt.registerTask('prod', ['less', 'ngtemplates', 'concat:prod', 'uglify', 'clean']);
+    grunt.registerTask('version', ['ngtemplates', 'concat:version', 'uglify:version', 'clean']);
 }
