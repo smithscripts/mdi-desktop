865f5fa1a5b035306eb04c0cab64181d8ca925a9
robsmithers <rob.smith@outlook.com>
2014-02-05 19:25:26 -0600

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         concat: {
-            options: {
-                // define a string to put between each file in the concatenated output
-                separator: ';'
-            },
-            dist: {
-                // the files to concatenate
-                src: ['src/js/**/*.js'],
-                // the location of the resulting JS file
-                dest: '<%= pkg.name %>.js'
-            }
+            'dist/all.js': ['src/js/**/*.js']
         },
         // Test settings
         karma: {
